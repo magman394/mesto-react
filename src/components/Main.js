49f4164b8ee2a35e36b58d892536c563468be115
Ilia Belokopytov <thecapitalB@yandex.ru>
2021-06-19 00:37:02 +0300

    feat: 10 работа остался имдж попап

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 import Avatar from '../images/avatar-image.jpg';
 import ProfileEditBotton from '../images/edit-botton.svg';
 import ProfileAddBotton from '../images/add-botton.svg';
-import PopupCloseIcon from '../images/Close-Icon.svg';
+
+import PopupWithForm from './PopupWithForm';
 
 function handleEditAvatarClick() {
   document.querySelector('#popupAvatar').classList.add('popup_is-opened');
@@ -31,60 +32,32 @@ function Main() {
           </section>
           <section class="elements">
           </section>
-          <div class="popup" id="popupAutor">
-          <div class="popup__container">
-            <button type="button" class="popup__close opacity-link"><img src={PopupCloseIcon} alt="кнопка закрытия"/></button>
-            <form class="popup__form" name="popup-profile" id="formAutor" novalidate>
-              <h2 class="popup__title popup__title_color_black">Редактировать профиль</h2>
-              <input name="firstName" id="firstName" type="text" class="popup__input" minlength="2" maxlength="40" required placeholder="Имя"/>
-              <span id="firstName-error" class="popup__error"></span>
-              <input name="lastName" id="lastName" type="text" class="popup__input" minlength="2" maxlength="200" required placeholder="Профессия"/>
-              <span id="lastName-error" class="popup__error"></span>
-              <button id="saveAutor" type="submit" class="popup__submit">Сохранить</button>
-            </form>
-          </div>
-        </div>
-        <div class="popup" id="popupCard">
-          <div class="popup__container">
-            <button type="button" class="popup__close opacity-link" id="closeCard"><img src={PopupCloseIcon} alt="кнопка закрытия"/></button>
-            <form name="popup-profile" id="formCards" novalidate>
-              <h2 class="popup__title popup__title_color_black">Новое место</h2>
-              <input name="name" type="text" class="popup__input" minlength="2" maxlength="30" required placeholder="Название" id="inputTitle"/>
-              <span id="inputTitle-error" class="popup__error"></span>
-              <input name="link" id="inputLink" type="url" class="popup__input" required placeholder="Ссылка на картинку"/>
-              <span id="inputLink-error" class="popup__error"></span>
-              <button id="saveCard" type="submit" class="popup__submit">Сохранить</button>
-            </form>
-          </div>
-        </div>
-        <div class="popup" id="popupImage">
-          <div class="popup__image-container">
-            <button type="button" class="popup__close popup__close_image opacity-link"><img src={PopupCloseIcon} alt="кнопка закрытия"/></button>
-            <img class="popup__image" src="#" alt="фото"/>
-            <h2 class="popup__title" id="popupTtl"></h2>
-          </div>
-        </div>
-        <div class="popup" id="popupDelCard">
-          <div class="popup__container">
-            <form class="popup__form-avatar" id="formDel">
-            <h2 class="popup__title popup__title_not_margin">Вы уверены?</h2>
-            <button type="button" class="popup__close popup__close_cards opacity-link"><img src={PopupCloseIcon} alt="кнопка закрытия"/></button>
-            <button id="deleteCard" type="submit" class="popup__submit popup__submit_mini_margin">Да</button>
-            </form>
-          </div>
-        </div>
-
-      <div class="popup" id="popupAvatar">
-        <div class="popup__container">
-          <button type="button" class="popup__close opacity-link" id="closeAvatar"><img src={PopupCloseIcon} alt="кнопка закрытия"/></button>
-          <form class="popup__form-avatar" id="formAvatar" novalidate>
-            <h2 class="popup__title popup__title_not_margin">Обновить аватар</h2>
+        <PopupWithForm popupid="popupAutor" formid="formAutor" buttonid="saveAutor" title="Редактировать профиль" classtype="popup__title_color_black">
+          <input name="firstName" id="firstName" type="text" class="popup__input" minlength="2" maxlength="40" required placeholder="Имя"/>
+          <span id="firstName-error" class="popup__error"></span>
+          <input name="lastName" id="lastName" type="text" class="popup__input" minlength="2" maxlength="200" required placeholder="Профессия"/>
+          <span id="lastName-error" class="popup__error"></span>
+        </PopupWithForm>
+        <PopupWithForm popupid="popupCard" formid="formCards" buttonid="saveCard" title="Новое место" classtype="popup__title_color_black">
+          <input name="name" type="text" class="popup__input" minlength="2" maxlength="30" required placeholder="Название" id="inputTitle"/>
+          <span id="inputTitle-error" class="popup__error"></span>
+          <input name="link" id="inputLink" type="url" class="popup__input" required placeholder="Ссылка на картинку"/>
+          <span id="inputLink-error" class="popup__error"></span>
+        </PopupWithForm>
+        <PopupWithForm popupid="popupDelCard" formid="formDel" buttonid="deleteCard" title="Вы уверены?" classtype="popup__title_not_margin">
+          <input name="name" type="text" class="popup__input" minlength="2" maxlength="30" required placeholder="Название" id="inputTitle"/>
+          <button id="deleteCard" type="submit" class="popup__submit popup__submit_mini_margin">Да</button>
+        </PopupWithForm>
+        <PopupWithForm popupid="popupAvatar" formid="closeAvatar" buttonid="saveAvatar" title="Обновить аватар" classtype="popup__title_not_margin">
             <input name="link" id="AvatarLink" type="url" class="popup__input" required placeholder="Ссылка на картинку"/>
             <span id="AvatarLink-error" class="popup__error"></span>
-            <button id="saveAvatar" type="submit" class="popup__submit">Сохранить</button>
-          </form>
+        </PopupWithForm>
+        <div class="popup" id="popupImage">
+          <div class="popup__image-container">
+              <img class="popup__image" src="#" alt="фото"/>
+              <h2 class="popup__title" id="popupTtl"></h2>
+          </div>
         </div>
-      </div>
         </main>
         
   );
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PopupCloseIcon from '../images/Close-Icon.svg';
+const PopupWithForm = (props) => {
+    return (
+        <div class="popup" id={props.popupid} >
+          <div class="popup__container">
+            <button type="button" class="popup__close opacity-link"><img src={PopupCloseIcon} alt="кнопка закрытия"/></button>
+            <form class="popup__form" name="popup-profile" id={props.formid} novalidate>
+              <h2 class={`popup__title ${props.classtype}`}>{props.title}</h2>
+              {props.children}
+              <button id={props.buttonid} type="submit" class="popup__submit" >Сохранить</button>
+            </form>
+          </div>
+        </div>
+    );
+  }
+  export default PopupWithForm;
