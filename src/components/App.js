b8373e7499d95f0c65a4648804a6f62ec5ce9cd4
Ilia Belokopytov <thecapitalB@yandex.ru>
2021-06-22 01:53:36 +0300

    feat: закрытие попапов

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,29 @@
+// import React from "react";
+// import Header from './Header';
+// import Main from './Main';
+// import Footer from './Footer';
+// import PopupWithForm from './PopupWithForm';
+
+
+// function App() {
+
+//   return (
+//     <>
+//       <div className="page">
+//         <Header />
+//         <Main 
+//         onEditProfile="handleEditProfileClick"
+//         onAddPlace="handleAddPlaceClick"
+//         onEditAvatar="handleEditAvatarClick"
+//         />
+//         <Footer />
+//         <PopupWithForm />
+//       </div>
+//    </>
+//   );
+// }
+
+// export default App;
 import React from "react";
 import Header from './Header';
 import Main from './Main';
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,18 +1,48 @@
+// import React from "react";
+// import PopupCloseIcon from '../images/Close-Icon.svg';
+// const PopupWithForm = ({popupid, formid, classtype, title, children, buttonid}) => {
+//   const [isClose, setIsClose] = React.useState();
+
+//   React.useEffect(() => {
+//     setIsClose(false)
+//   }, [isClose]);
+  
+//     return (
+//         <div className={`popup${isClose ? ' popup_is-opened' : ''}`} id={popupid} >
+//           <div className="popup__container">
+//             <button type="button" className="popup__close opacity-link"><img src={PopupCloseIcon} onClick={() => {setIsClose(true)}} alt="кнопка закрытия"/></button>
+//             <form className="popup__form" name="popup-profile" id={formid} noValidate>
+//               <h2 className={`popup__title ${classtype}`}>{title}</h2>
+//               {children}
+//               <button id={buttonid} type="submit" className="popup__submit" >Сохранить</button>
+//             </form>
+//           </div>
+//         </div>
+//     );
+//   }
+//   export default PopupWithForm;
+
 import React from "react";
 import PopupCloseIcon from '../images/Close-Icon.svg';
-const PopupWithForm = (props) => {
+const PopupWithForm = ({popupid, formid, classtype, title, children, buttonid}) => {
+  const [isClose, setIsClose] = React.useState();
 
+  React.useEffect(() => {
+    setIsClose(false)
+  }, [isClose]);
+  
     return (
-        <div className="popup" id={props.popupid} >
+        <div className={`popup${isClose ? ' popup_is-opened' : ''}`} id={popupid} >
           <div className="popup__container">
-            <button type="button" className="popup__close opacity-link"><img src={PopupCloseIcon} alt="кнопка закрытия"/></button>
-            <form className="popup__form" name="popup-profile" id={props.formid} noValidate>
-              <h2 className={`popup__title ${props.classtype}`}>{props.title}</h2>
-              {props.children}
-              <button id={props.buttonid} type="submit" className="popup__submit">Сохранить</button>
+            <button type="button" className="popup__close opacity-link"><img src={PopupCloseIcon} onClick={() => {setIsClose(true)}} alt="кнопка закрытия"/></button>
+            <form className="popup__form" name="popup-profile" id={formid} noValidate>
+              <h2 className={`popup__title ${classtype}`}>{title}</h2>
+              {children}
+              <button id={buttonid} type="submit" className="popup__submit" >Сохранить</button>
             </form>
           </div>
         </div>
     );
   }
   export default PopupWithForm;
+
