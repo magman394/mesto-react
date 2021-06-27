9d0f83a84526b0d37b8d0d79fe743b6420e6bd6f
Ilia Belokopytov <thecapitalB@yandex.ru>
2021-06-27 20:49:03 +0300

    fix: api

--- src/utils/API.js
+++ src/utils/API.js
@@ -21,13 +21,14 @@ class API  {
 }
 
 
-const Api = new API({
+const config = {
   url: 'https://mesto.nomoreparties.co/v1/cohort-24/',
   token: {
     authorization: 'b94e78d1-b2d6-4481-aa74-fc7e4dc8c239',
     "Content-Type": "application/json"
   }
- });
+ }
+const Api = new API(config);
 
 export default Api;
 
